92312cc79473ef971bbef00ed616f92ce6bd88a5
Rehan Manzoor <caprichano@gmail.com>
2017-02-04 18:25:44 +0400

    Added operators file

--- basic-functions.R
+++ basic-functions.R
@@ -1,9 +1,12 @@
+
+# just a basic function which create a 100 random normal integers and calculate the mean of these functions and print it.
 myfunction <- function(){
   x = rnorm(100)
   y = mean(x)
   print(y)
 }
 
+# this function read a csv file, it takes a parameter which is file name and read the file.
 readCsv  <- function(name){
   read.csv(name)
 }
---
+++ operators.R
@@ -0,0 +1,7 @@
+# the statement start with # sign tells the R engine this is a comment.
+
+
+# <- is an Assignment Operator
+
+myName <- 'rummykhan'
+print(myName)
