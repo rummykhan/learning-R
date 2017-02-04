4a266f0f33b2f03344542b78d82d53db5fa189e2
Rehan Manzoor <caprichano@gmail.com>
2017-02-04 19:31:55 +0400

    learning R helpers

--- helpers.R
+++ helpers.R
@@ -1,4 +1,4 @@
-
+# Creating Vectors
 # c() function is concatenate function (it can be used to create vectors)
 
 x <- c( 0.5, 0.6 )
@@ -13,3 +13,21 @@ x <- c(T, F)
 # creating a vector
 
 x <- vector('numeric', length=20)
+
+# We can concatenate different class objects as well..
+# then system try to manage the types into One.. as these are vectors and vectors value should be all of same types
+# this is what is called coercion 
+# e.g.
+
+x <- c(TRUE, 0.5)
+
+# Will result x = 1.0, 0.5
+
+
+# Then there is Explicit Coercion
+
+as.character(x)
+
+# will output '1.0', '0.5'
+
+
