5cdd3105cc29a0686697c64c984ac40de21a6f49
Rehan Manzoor <caprichano@gmail.com>
2017-02-04 19:25:09 +0400

    Added helpers.R

---
+++ helpers.R
@@ -0,0 +1,15 @@
+
+# c() function is concatenate function (it can be used to create vectors)
+
+x <- c( 0.5, 0.6 )
+
+x <- c(TRUE, FALSE)
+
+# TRUE shorthand format is T
+# FALSE shorthand format is F
+
+x <- c(T, F)
+
+# creating a vector
+
+x <- vector('numeric', length=20)
